87c3d4f9a3f80e05f32feaf9f60d9dc6ecb3cef3
Utsav Singh <89920027+Utsav-Singh-01@users.noreply.github.com>
2024-02-13 23:00:15 +0530

    learning the lvl1 and lvl2 of JS wk1

--- week-1/offline-class-1/level-2/01-Class.js
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -1,12 +1,17 @@
-
 class Animal {
-  constructor(name, legCount) {
-    this.name = name
-    this.legCount = legCount
-  }
-  describe() {
-    return `${this.name} has ${this.legCount} legs`
-  }
+    constructor(name, legCount) {
+        this.name = name;
+        this.legCount = legCount;
+    }
+
+    static myType = () => console.log("Animal");
+
+    describe() {
+        return `${this.name} has ${this.legCount} legs`;
+    }
 }
 
+const myObj = new Animal("Buddy", 4);
+console.log(myObj.describe());
 
+Animal.myType();
